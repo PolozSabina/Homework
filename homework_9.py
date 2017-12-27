48b64f9c9e8084a586a50c79c0fa6de0da553a28
Sabina <zaxidova.sabina@gmail.com>
2017-12-27 22:59:46 +0200

    Homework 9 - change name news to new_s

--- homework_9.py
+++ homework_9.py
@@ -3,8 +3,8 @@ var = s.split('_')
 word_1 = var[0]
 word_2 = var[1]
 word_3 = var[2]
-news = word_1.capitalize() + word_2.capitalize() + word_3.capitalize()
-print('В формате CamelizedStyl: %s' %  news)
+new_s = word_1.capitalize() + word_2.capitalize() + word_3.capitalize()
+print('В формате CamelizedStyl: %s' %  new_s)
 
 
 
