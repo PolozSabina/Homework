088e63e151330c09f002467ce9374b3a329c502d
Sabina <zaxidova.sabina@gmail.com>
2018-01-12 17:16:20 +0200

    Homework 13

--- homework_13.py
+++ homework_13.py
@@ -1,12 +1,12 @@
 import math
 
-input_a = input('Enter base triangle')
-input_b = input('Enter height of triangle')
+input_a = int(input('Enter base triangle'))
+input_b = int(input('Enter height of triangle'))
 
 def triangle_square_and_perimeter(a, b):
-    square = int (1/2 * int(a)*int(b))
-    hypotenuse = math.sqrt(int(a)**2 + int(b)**2)
-    perimetr = int(a) + int(b) + int(hypotenuse)
+    square = 1/2 * a*b
+    hypotenuse = math.sqrt(a**2 + b**2)
+    perimetr = a + b + hypotenuse
     return square, perimetr
 
 print(triangle_square_and_perimeter(input_a, input_b))
