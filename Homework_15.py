1af9b56e375049a070c096f970c8fe0c8c8f0ff5
Sabina <zaxidova.sabina@gmail.com>
2018-01-17 11:58:48 +0200

    Homework 15

--- Homework_15.py
+++ Homework_15.py
@@ -10,8 +10,8 @@ r2 = 5
 
 def cirles_intersects(x1, y1, r1, x2, y2, r2):
     dist = math.sqrt((x2 - x1)**2 +(y2 - y1)**2)
-    suma_R = r1 + r2
-    return dist <= suma_R and dist >= abs(r1-r2)
+    sum__r = r1 + r2
+    return dist <= sum__r and dist >= abs(r1-r2)
 
 if cirles_intersects(x1, y1, r1, x2, y2, r2):
     print('Oкружности пересекаются')
