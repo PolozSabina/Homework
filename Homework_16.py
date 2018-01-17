81403e5292e71a398eb127da4c38a43fe81899aa
Sabina <zaxidova.sabina@gmail.com>
2018-01-17 12:01:18 +0200

    Homework 16

--- Homework_16.py
+++ Homework_16.py
@@ -1,13 +1,12 @@
-v1 = 12
+v1 = 13
 v2 = 18
 
-
 def have_trains_crashed(v1, v2):
-    train_A = 4
-    train_B = 6
-    time_train_A = train_A / v1
-    time_train_B = train_B / v2
-    return time_train_B <= time_train_A
+    distance_train__a = 4
+    distance_train__b = 6
+    time_train__a = distance_train__a / v1
+    time_train__b = distance_train__b / v2
+    return time_train__b <= time_train__a
 
 if have_trains_crashed(v1, v2):
     print('Поезда столкнутся')
