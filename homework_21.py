7652ff7e3c6bb65f8ec9e536aadbc2f492e8a6b5
Sabina <zaxidova.sabina@gmail.com>
2018-01-25 22:37:44 +0200

    Homework 21

--- homework_21.py
+++ homework_21.py
@@ -1,18 +1,13 @@
-#Написать функцию возвращающую наибольшую цифру случайно сгенерированного 12 ти-значного натурального числа.
 import random
 
-n = 123456789123
-
 def get_max_digit(number):
-    n = str(number)
-    rand_lst = [0]*len(n)
-
-    for i in range(len(n)):
-        rand_lst[i] = n[i:i+1]
-
+    n_lst = str(number)
+    rand_lst = [i for i in n_lst]
     max_value = int(max(rand_lst))
-
     return max_value
 
+n = random.randint(100000000000, 999999999999)
+print(n)
 
 print(get_max_digit(n))
+
