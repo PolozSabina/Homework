55c026e2e50558ea460aadc4d32faaf9fa11dae6
Sabina <zaxidova.sabina@gmail.com>
2018-01-27 12:52:00 +0200

    Homework 20

--- homework_20.py
+++ homework_20.py
@@ -1,17 +1,17 @@
 import random
 
 def diff_even_odd(num_limit, lower_bound, upper_bound):
-    random_vars = []
+    random_var = []
     even = 0
     odd = 0
 
     for i in range(num_limit):
-        random_vars = random.randint(lower_bound, upper_bound)
+        random_var = random.randint(lower_bound, upper_bound)
 
-        if random_vars % 2 == 0:
-            even += random_vars
+        if random_var % 2 == 0:
+            even += random_var
         else:
-            odd += random_vars
+            odd += random_var
 
         res = even - odd
         return res
